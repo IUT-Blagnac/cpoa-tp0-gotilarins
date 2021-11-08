6af4ca65de6c6640d6b2f401453e56b36c91ccf2
Jérémy.SEGUR <60040030+gotilarins@users.noreply.github.com>
2021-11-08 21:22:22 +0100

    Add files via upload
    Fin du TP0 de CPOA.
    Beaucoup de difficulté à installer et à configurer Maven 3. Les commende mvn ne fonctionne pas dans mon GitBash malgré une installation de Maven3 correcte.

---
+++ src/main/java/Order.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.LinkedList;
+
+public class Order {
+    private String owner ;
+    public void setOwner(String who) { this.owner = who; }
+    private String recipient;
+    public void setRecipient(String who) { this.recipient = who; }
+    @Override
+    public String toString() {
+        return "Order: " + owner + " / " + recipient + " / { " + contents + "}";
+    }
+    private List<Drink> contents = new LinkedList<>();
+    public List<Drink> getDrinks() { return contents; }
+    static class Drink {
+        public Drink(String name){ this.name = name; }
+        private String name;
+        public String getName() { return name; }
+        @Override public String toString() { return name; }
+    }
+
+}
